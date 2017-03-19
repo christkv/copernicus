f4130ef452c0f7a635f4c69e4e6e55f5eb6e7e6e
Christian Kvalheim <christkv@gmail.com>
2017-03-19 14:30:01 +0100

    ported metadata

--- shared/lib/schemas/metadata/metadata.js
+++ shared/lib/schemas/metadata/metadata.js
@@ -1,8 +1,5 @@
 "use strict";
 
-var f = require('util').format,
-  co = require('co');
-
 /*
  * Create a new metadata instance
  */
@@ -16,29 +13,21 @@ class MetaData {
   /*
    * Create a new metadata document on mongodb
    */
-  create() {
-    var self = this;
-
-    return new Promise(function(resolve, reject) {
-      co(function* () {
-        // Insert the metadata
-        yield self.metadatas.insertOne({
-            _id: self.id
-          , metadata: self.metadata
-        });
-
-        resolve(self);
-      }).catch(reject);
+  async create() {
+    // Insert the metadata
+    await this.metadatas.insertOne({
+        _id: this.id
+      , metadata: this.metadata
     });
+
+    return this;
   }
 
   /*
    * Search using metadata fields
    */
-  static findByFields(collections, fields, options) {
-    var self = this;
+  static async findByFields(collections, fields, options = {}) {
     var queryParts = [];
-    options = options || {};
 
     for(var name in fields) {
       queryParts.push({$elemMatch: {key: name, value: fields[name] }});
@@ -55,29 +44,18 @@ class MetaData {
       cursor.setReadPreference(options.readPreference);
     }
 
-    return new Promise(function(resolve, reject) {
-      co(function* () {
-        // Execute the query
-        var docs = yield cursor.toArray();
-        docs.map(function(x) {
-          return new MetaData(collections, x._id, x.metadata);
-        });
-
-        resolve(docs);
-      }).catch(reject);
+    // Execute the query
+    var docs = await cursor.toArray();
+    return docs.map((x) => {
+      return new MetaData(collections, x._id, x.metadata);
     });
   }
 
   /*
    * Create the optimal indexes for the queries
    */
-  static createOptimalIndexes(collections) {
-    return new Promise(function(resolve, reject) {
-      co(function* () {
-        yield collections['metadatas'].ensureIndex({"metadata.key": 1, "metadata.value": 1});
-        resolve();
-      }).catch(reject);
-    });
+  static async createOptimalIndexes(collections) {
+    await collections['metadatas'].ensureIndex({"metadata.key": 1, "metadata.value": 1});
   }
 }
 
--- shared/test/metadata_tests.js
+++ shared/test/metadata_tests.js
@@ -1,74 +1,61 @@
 "use strict";
 
-var co = require('co');
+const assert = require('assert');
+const {
+  ObjectId,
+  MongoClient
+} = require('mongodb');
+const Metadata = require('../lib/schemas/metadata/metadata')
 
-var setup = function(db) {
-  var Metadata = require('../../lib/common/schemas/metadata/metadata');
-
+async function setup(db) {
   // All the collections used
   var collections = {
     metadatas: db.collection('metadatas')
   }
 
-  return new Promise(function(resolve, reject) {
-    co(function* () {
-      try { yield collections['metadatas'].drop(); } catch(err) {};
-      yield Metadata.createOptimalIndexes(collections);
-      resolve();
-    }).catch(reject);
+  try { await collections['metadatas'].drop(); } catch(err) {}
+
+  await Metadata.createOptimalIndexes(collections);
+}
+
+describe('Metadata', () => {
+  it('Correctly random metadata and query by metadata field', async () => {
+    var db = await MongoClient.connect('mongodb://localhost:27017/test');
+
+    // All the collections used
+    var collections = {
+      metadatas: db.collection('metadatas')
+    }
+
+    // Cleanup
+    await setup(db);
+
+    // Create metadata instance
+    var metadata1 = new Metadata(collections, new ObjectId(), [
+        { key: 'name', value: 'test image' }
+      , { key: 'type', value: 'image' }
+      , { key: 'iso', value: 100 }
+    ]);
+
+    // Create metadata instance
+    var metadata2 = new Metadata(collections, new ObjectId(), [
+        { key: 'name', value: 'test image 2' }
+      , { key: 'type', value: 'image' }
+      , { key: 'iso', value: 200 }
+    ]);
+
+    // Create metadata instance
+    await metadata1.create();
+    await metadata2.create();
+
+    // Locate by single metadata field
+    var items = await Metadata.findByFields(collections, {type: 'image'});
+    assert.equal(2, items.length);
+
+    // Locate by multiple metadata fields
+    var items = await Metadata.findByFields(collections, {type: 'image', iso: 100});
+    assert.equal(1, items.length);
+
+    db.close();
   });
-}
-
-exports['Correctly random metadata and query by metadata field'] = {
-  metadata: { requires: { } },
-
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var Metadata = require('../../lib/common/schemas/metadata/metadata')
-      , ObjectId = require('mongodb').ObjectId
-      , MongoClient = require('mongodb').MongoClient;
-
-    co(function* () {
-      var db = yield MongoClient.connect(configuration.url());
-
-      // All the collections used
-      var collections = {
-        metadatas: db.collection('metadatas')
-      }
-
-      // Cleanup
-      yield setup(db);
-
-      // Create metadata instance
-      var metadata1 = new Metadata(collections, new ObjectId(), [
-          { key: 'name', value: 'test image' }
-        , { key: 'type', value: 'image' }
-        , { key: 'iso', value: 100 }
-      ]);
-
-      // Create metadata instance
-      var metadata2 = new Metadata(collections, new ObjectId(), [
-          { key: 'name', value: 'test image 2' }
-        , { key: 'type', value: 'image' }
-        , { key: 'iso', value: 200 }
-      ]);
-
-      // Create metadata instance
-      yield metadata1.create();
-      yield metadata2.create();
-
-      // Locate by single metadata field
-      var items = yield Metadata.findByFields(collections, {type: 'image'});
-      test.equal(2, items.length);
-
-      // Locate by multiple metadata fields
-      var items = yield Metadata.findByFields(collections, {type: 'image', iso: 100});
-      test.equal(1, items.length);
-
-      db.close();
-      test.done();
-    }).catch(function(err) {
-      process.nextTick(function() {throw err});
-    });
-  }
-}
+});
